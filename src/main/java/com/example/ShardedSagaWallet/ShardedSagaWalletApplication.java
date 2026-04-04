525298e2ef69f5e03e2d8dffb0ede15f74477831
krishna <krishna_a@ce.iitr.ac.in>
2026-04-04 14:07:34 +0530

    Load sharding config values from .env

--- src/main/java/com/example/ShardedSagaWallet/ShardedSagaWalletApplication.java
+++ src/main/java/com/example/ShardedSagaWallet/ShardedSagaWalletApplication.java
@@ -7,15 +7,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShardedSagaWalletApplication {
 
-    private static void loadEnvironmentVariables(){
-        Dotenv dotenv = Dotenv.configure().load();
+    private static void loadEnvironmentVariables() {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        // 1. Load all .env variables into System properties
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        /*
+            Sharding sphere can't access the env variables, it can system variables.
+        */
+        // 2. Fallback: load all OS env variables if not already set
+        System.getenv().forEach((key, value) -> {
+            if (System.getProperty(key) == null) {
+                System.setProperty(key, value);
+            }
+        });
     }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         loadEnvironmentVariables();
-
         SpringApplication.run(ShardedSagaWalletApplication.class, args);
-	}
-
+    }
 }
