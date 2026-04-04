ffd3f180792fe8f900936336495149b0a19ef0a1
krishna <krishna_a@ce.iitr.ac.in>
2026-04-04 18:36:06 +0530

    added status methods in SagaInstance and SagaStep entities

--- src/main/java/com/example/ShardedSagaWallet/entities/saga/SagaInstance.java
+++ src/main/java/com/example/ShardedSagaWallet/entities/saga/SagaInstance.java
@@ -1,6 +1,7 @@
 package com.example.ShardedSagaWallet.entities.saga;
 
 import com.example.ShardedSagaWallet.enums.SagaStatus;
+import com.example.ShardedSagaWallet.enums.SagaSteps;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,5 +31,29 @@ public class SagaInstance {
     private String context;
 
     @Column(name = "current_step")
-    private String currentStep;
+    private SagaSteps currentStep;
+
+    public void markAsStarted() {
+        this.status = SagaStatus.STARTED;
+    }
+
+    public void markAsRunning() {
+        this.status = SagaStatus.RUNNING;
+    }
+
+    public void markAsCompleted() {
+        this.status = SagaStatus.COMPLETED;
+    }
+
+    public void markAsFailed() {
+        this.status = SagaStatus.FAILED;
+    }
+
+    public void markAsCompensating() {
+        this.status = SagaStatus.COMPENSATING;
+    }
+
+    public void markAsCompensated() {
+        this.status = SagaStatus.COMPENSATED;
+    }
 }
