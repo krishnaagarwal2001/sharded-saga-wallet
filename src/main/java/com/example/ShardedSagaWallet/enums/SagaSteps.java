0b5110bb802331e73f5fe4100880c2ce1566b0d0
krishna <krishna_a@ce.iitr.ac.in>
2026-04-04 19:29:13 +0530

    Fixed Saga Steps ordering

--- src/main/java/com/example/ShardedSagaWallet/enums/SagaSteps.java
+++ src/main/java/com/example/ShardedSagaWallet/enums/SagaSteps.java
@@ -1,7 +1,7 @@
 package com.example.ShardedSagaWallet.enums;
 
 public enum SagaSteps {
-    CREDIT_DESTINATION_WALLET_STEP,
     DEBIT_SOURCE_WALLET_STEP,
+    CREDIT_DESTINATION_WALLET_STEP,
     UPDATE_TRANSACTION_STATUS_STEP,
 }
